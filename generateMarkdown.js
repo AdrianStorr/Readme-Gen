7b4e934bc2d43993a949316815fe1f8106738f87
adrian <68280694+AdrianStorr@users.noreply.github.com>
2020-10-21 14:37:45 -0400

    Add files via upload

---
+++ generateMarkdown.js
@@ -0,0 +1,43 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+
+
+  ## Table of Contents
+  *[Description](Description)
+  *[Usage](Usage)
+  *[Installation](Installation)
+  *[License](License)
+  *[Contributors](contributers)
+  *[Test](Test)
+  *[Questions](Questions)
+
+  ##Description
+  ${data.Description}
+
+  ##Installation Instuctions
+  ${data.Installation}
+
+  ##Usage Information
+  ${data.Usage}
+
+  ##License
+  ${data.License}
+
+  ##Contributers
+  ${data.Contribution}
+
+  ##Test
+  ${data.Test}
+
+  ##Question
+  Contact me:
+
+  Github:[${data.Username}](https://github.com/AdrianStorr)
+  
+  Email :[${data.Email}](https://github.com/AdrianStorr)
+`;}
+
+
+module.exports = generateMarkdown;
